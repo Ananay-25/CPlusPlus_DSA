3f3f79fea8fb3e8796ab6c9f14a90798d26f4dd4
Ananay Srivastava <ananaysrivas102@gmail.com>
2024-07-29 14:36:39 +0530

    brute

--- ZZZZ/max_subarray_w_equal_0_1.cpp
+++ ZZZZ/max_subarray_w_equal_0_1.cpp
@@ -1,7 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int length_max_subarray(vector<int> arr){
+
+    int max_len=0;
+    int n=arr.size();
+
+    for(int i=0;i<n-1;i++){
+
+        for(int j=i+1;j<n;j++){
+
+            int count0=0;
+            int count1=0;
+            for(int z=i;z<=j;z++){
+                if(arr[z]==1) count1++;
+                else count0++;
+            }
+            int len=j-i+1;
+            if(count0== count1 && len>=max_len){
+                max_len=len;
+            }
+
+        }
+    }
+    return max_len;
+
+
+}
+
 int main()
 {
+    vector<int> arr={0,0,0,1};
+    cout<<length_max_subarray(arr);
     return 0;
 }
